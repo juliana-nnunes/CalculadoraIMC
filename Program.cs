5b5c755a0a6f94b59b6b4b1f08299d597543d633
juliana-nnunes <josecarloslimanunes30@gmail.com>
2025-11-02 17:27:47 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿double massa, altura, imc;
+Console.WriteLine("-- Calculadora IMC --/n");
+Console.Write("Digite seu peso em kg...:" );
+massa = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite sua altura em m..:");
+altura = Convert.ToDouble(Console.ReadLine());
+
+imc = massa / Math.Pow(altura, 2);
+
+Console.WriteLine($"Seu IMC é {imc:N2} kg/m²");
+
